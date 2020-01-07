145f92bb859e50ff4d7284b95a51559e7d125c45
jlelito <joshualelito@gmail.com>
2020-01-07 18:18:57 -0500

    Updates

---
+++ precache-manifest.7f06ca6203cb16245c7a7065c9468bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066572435ef45c17a65038f791d72fdf",
+    "url": "/events-dapp/index.html"
+  },
+  {
+    "revision": "20c2fec26a166596c003",
+    "url": "/events-dapp/static/css/2.c0f5ac91.chunk.css"
+  },
+  {
+    "revision": "20c2fec26a166596c003",
+    "url": "/events-dapp/static/js/2.2a1f0042.chunk.js"
+  },
+  {
+    "revision": "99c5655d2432b8199a89e031b7b8d3f7",
+    "url": "/events-dapp/static/js/2.2a1f0042.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3cb8aa95f6184e000f3d",
+    "url": "/events-dapp/static/js/main.74800806.chunk.js"
+  },
+  {
+    "revision": "03507f258f140cbd5dad",
+    "url": "/events-dapp/static/js/runtime-main.179b7b9c.js"
+  }
+]);
